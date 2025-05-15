d0ae1e6b060b112bbb3132e775476eeba42e8cbd
dongxiaolong <17695564750@qq.com>
2025-05-16 00:22:31 +0800

    添加登录界面

--- flutter/lib/login_page.dart
+++ flutter/lib/login_page.dart
@@ -1,27 +1,11 @@
-import 'dart:io';
-import 'package:encrypt/encrypt.dart';
-import 'package:flutter/services.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:encrypt/encrypt.dart' as Encrypt;
-import 'package:flutter/material.dart' hide Key;
-import 'package:go_router/go_router.dart';
-import 'package:zy_app/bean/app_version_bean.dart';
-import 'package:zy_app/bean/login_bean.dart';
-import 'package:zy_app/bean/title_bean.dart';
-import 'package:zy_app/bean/user_info_bean.dart';
-import 'package:zy_app/const/conf_encode.dart';
-import 'package:zy_app/const/const_file.dart';
-import 'package:zy_app/const/const_net.dart';
-import 'package:zy_app/const/const_sp.dart';
-import 'package:zy_app/main.dart';
-import '../bean/const_file_bean.dart';
-import '../const/const_image.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_app_update/flutter_app_update.dart';
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart' hide Key;
+import 'package:flutter_hbb/mobile/pages/home_page.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import '../utils/sp_util.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -30,7 +14,6 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-final dio = Dio();
 var _inputAccount = '';
 var _inputPwd = '';
 
@@ -48,41 +31,6 @@ class _LoginPageState extends State<LoginPage> {
   late PackageInfo _packageInfo ;
 
 
-  /// 背景
-  final Widget _topBg = Container(
-    height: 270,
-    width: double.infinity,
-    decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage(ImageConstant.loginBg), fit: BoxFit.cover)),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: const Color(0xfff6f3f3),
-          ),
-          alignment: Alignment.center,
-          height: 60,
-          width: 60,
-          child: Image.asset(
-            ImageConstant.logo,
-            height: 50,
-            width: 50,
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text(
-          '准研上岸！',
-          style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.w700, fontSize: 16),
-        )
-      ],
-    ),
-  );
 
 
   @override
@@ -95,135 +43,34 @@ class _LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     super.initState();
-    _loadShare();
-    _initPackageInfo();
 
   }
 
-  //初始化包信息
-  Future<void> _initPackageInfo() async {
-    final info = await PackageInfo.fromPlatform();
-    setState(() {
-      _packageInfo = info;
-      _showUpdateDialog(true);
-    });
-  }
-
-  //显示更新弹窗
-  _showUpdateDialog(bool forcedUpgrade) async {
-    await dio.post('${NetConst.getNewApk}/${_packageInfo.buildNumber}').then((response){
-      Map<String, dynamic> map = jsonDecode(response.toString());
-      var versionBean = AppVersionBean.fromJson(map);
-      if(versionBean.data==null||versionBean.data!.url==null||versionBean.data!.content==null){
-        return;
-      }
-      showDialog(
-        context: context,
-        barrierDismissible: !forcedUpgrade,
-        builder: (BuildContext context) {
-          return WillPopScope(
-            onWillPop: () => Future.value(!forcedUpgrade),
-            child: AlertDialog(
-              title: const Text('发现新版本'),
-              content: Text(versionBean.data!.content!.replaceAll("\\n", "\n")),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('升级'),
-                  onPressed: () {
-                    _appUpdate(versionBean);
-                    if (!forcedUpgrade) {
-                      Navigator.of(context).pop();
-                    }
-                  },
-                ),
-              ],
-            ),
-          );
-        },
-      );
-    });
-  }
-
-  _appUpdate(AppVersionBean bean) {
-    UpdateModel model = UpdateModel(
-      NetConst.base_url+bean.data!.url!,
-      "准妍计算机复试V${bean.data!.versionId!}.apk",
-      "ic_launcher",
-      'iosurl',
-    );
-    AzhonAppUpdate.update(model).then((value) => debugPrint('$value'));
-  }
-
-  //解析专题
-  void _analysisSubject(){
-    var subjectBeanListLength = SpUtils.getIntByBase64("subjectBeanListLength");
-    if(subjectBeanListLength!=0){
-      return;
-    }
-    //解密
-    final key = Encrypt.Key.fromUtf8(EncodeConf.fileKey);
-    final iv = IV.fromLength(16);
-    final encrypter = Encrypter(AES(key));
-    var codeStr = rootBundle.loadString(FileConstant.titleJson);
-    codeStr.then((code) {
-      String jsonStr = encrypter.decrypt64(code,iv: iv);
-      TitleBean titleBean = TitleBean.fromJson(jsonDecode(jsonStr));
-      for(int i=0;i<titleBean.subjectBeanList!.length;i++){
-        var subjectBeanList = titleBean.subjectBeanList![i];
-        var sid=subjectBeanList.sid;
-        for(int j=0;j<subjectBeanList.topicBeanList!.length;j++){
-          var topicBean = subjectBeanList.topicBeanList![j];
-          var tid=topicBean.tid!;
-
-          SpUtils.setBase64ByString('${tid}_title', topicBean.title!);
-          SpUtils.setBase64ByString('${tid}_answer', topicBean.answer!);
-          SpUtils.setBase64ByString('${tid}_explain', topicBean.explain!);
-        }
-
-        SpUtils.setBase64ByInt('${sid}_topicBeanListLength', subjectBeanList.topicBeanList!.length);
-      }
-      SpUtils.setBase64ByInt('subjectBeanListLength' ,titleBean.subjectBeanList!.length);
-
-
-    });
-  }
-
-  Future<void> login() async {
+  Future<void> login(context) async {
+    final snackBar = SnackBar(content: Text('登录成功'));
+    final snackBar2 = SnackBar(content: Text('登录失败'));
     ///点击登录
     Map<String, dynamic> map = {};
     map['username'] = _accountController.value.text;
     map['password'] = _pwdController.value.text;
-    final response = await dio.post(NetConst.login, data: map);
-    Map<String, dynamic> userMap = jsonDecode(response.toString());
-    var loginBean = LoginBean.fromJson(userMap);
-    if(loginBean.status!=null && loginBean.status==200){
-      _analysisSubject();
-      sp.setString(SpConst.username, _accountController.value.text);
-      sp.setString(SpConst.password, _pwdController.value.text);
-      context.go("/indexPage");
-      _getUserInfo();
+    final response = await http.post(
+      Uri.parse('http://118.178.186.181:8080/jwt/token'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(map), // 将Map转换为JSON字符串
+    );
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
+      return json.decode(response.body);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to send data: ${response.statusCode}')));
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
     }
-    // _writeWinFile();
   }
 
-  // 获取用户信息
-  Future<void> _getUserInfo() async {
-    FormData formData = FormData.fromMap({"username": _accountController.value.text});
-    final response = await dio.post(NetConst.getUserInfo,data: formData);
-    var userInfoBean = UserInfoBean.fromJson(jsonDecode(response.toString()));
-    sp.setString(SpConst.userId, userInfoBean.data!.id!);
-  }
-
-  // 初始化对象存储
-  Future<void> _loadShare() async {
-    sp = await SharedPreferences.getInstance();
-    _inputAccount=sp.getString(SpConst.username) ?? "";
-    _inputPwd=sp.getString(SpConst.password) ?? "";
-    _accountController.text=_inputAccount;
-    _pwdController.text=_inputPwd;
-    setState(() {
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -233,7 +80,6 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             Column(
               children: [
-                _topBg,
                 const SizedBox(
                   height: 20,
                 ),
@@ -311,7 +157,7 @@ class _LoginPageState extends State<LoginPage> {
                         )
                     ),
                     onPressed: () {
-                      login();
+                      login(context);
                     },
                     child: const Text('登录', style: TextStyle(color: Colors.white,),),
                   ),
